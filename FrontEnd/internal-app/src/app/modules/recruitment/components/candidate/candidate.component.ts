49d4daef638feb43213231829ff25ec7840fcb5f
dinhhung09138 <dinhhung09138@gmail.com>
2019-07-25 22:23:47 +0700

    Adding candidate module

---
+++ FrontEnd/internal-app/src/app/modules/recruitment/components/candidate/candidate.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruitment-candidate',
+  templateUrl: './candidate.component.html',
+  styleUrls: ['./candidate.component.css']
+})
+
+export class CandidateComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() { }
+}
