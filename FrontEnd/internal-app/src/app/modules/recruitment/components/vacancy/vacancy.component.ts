c35ba92ff25c2b7790336747da7eb792374a5801
dinhhung09138 <dinhhung09138@gmail.com>
2019-07-25 22:24:36 +0700

    Adding vacancy module

---
+++ FrontEnd/internal-app/src/app/modules/recruitment/components/vacancy/vacancy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruitment-vacancy',
+  templateUrl: './vacancy.component.html',
+  styleUrls: ['./vacancy.component.css']
+})
+
+export class VacancyComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() { }
+
+}
