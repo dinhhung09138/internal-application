81c8233d0e6a718fd89e30cbdc75e06f08391c5c
dinhhung09138 <dinhhung09138@gmail.com>
2019-07-26 08:10:50 +0700

    Add layout, left sidebar, footer, navigation component

---
+++ FrontEnd/internal-app/src/app/modules/shared/left-sidebar/left-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-sidebar',
+  templateUrl: './left-sidebar.component.html'
+})
+
+export class LeftSidebarComponent implements OnInit {
+
+  openNav = false;
+
+  constructor() { }
+
+  ngOnInit() { }
+}
