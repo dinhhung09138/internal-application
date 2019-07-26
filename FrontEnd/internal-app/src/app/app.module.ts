a68874a4117c5b060b20bcef104c86848afa7457
dinhhung09138 <dinhhung09138@gmail.com>
2019-07-26 14:15:33 +0700

    Add ng-bootstrap module into project

--- FrontEnd/internal-app/src/app/app.module.ts
+++ FrontEnd/internal-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,6 +22,7 @@ import { FooterComponent } from './modules/shared/footer/footer.commponent';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
