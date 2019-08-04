de1adcb4741483bb2fde045de9ab93ab0cf07b7e
dinhhung09138 <dinhhung09138@gmail.com>
2019-08-04 23:02:22 +0700

    Add loading module

---
+++ FrontEnd/internal-app/src/app/shared/components/container-loading/container-loading.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'container-loading',
+  templateUrl: './container-loading.component.html',
+  styleUrls: ['./container-loading.component.css']
+})
+
+export class ContainerLoading {
+
+}
