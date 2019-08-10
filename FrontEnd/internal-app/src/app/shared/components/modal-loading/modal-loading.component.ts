58446df2fbfaad418ab96bf7bcf1de5c32d8bc74
dinhhung09138 <dinhhung09138@gmail.com>
2019-08-10 22:03:19 +0700

    change location shareModule services. Add loading modal

---
+++ FrontEnd/internal-app/src/app/shared/components/modal-loading/modal-loading.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-loading',
+  templateUrl: './modal-loading.component.html',
+  styleUrls: ['./modal-loading.component.css']
+})
+
+export class ModalLoadingComponent { }
--- FrontEnd/internal-app/src/app/shared/shared.module.ts
+++ FrontEnd/internal-app/src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableSearchComponent } from 'src/app/shared/components/table-search/table-search.component';
 import { TableFooterComponent } from 'src/app/shared/components/table-footer/table-footer.component';
+import { ModalLoadingComponent } from './components/modal-loading/modal-loading.component';
 
 @NgModule({
   imports:[
     CommonModule,
   ],
   declarations: [
+    ModalLoadingComponent,
     TableSearchComponent,
     TableFooterComponent,
   ],
   exports: [
+    ModalLoadingComponent,
     TableSearchComponent,
     TableFooterComponent
   ],
