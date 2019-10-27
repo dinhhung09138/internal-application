0602489539b6d6b9d461607d0eec040e81b7a0a6
dinhhung09138 <dinhhung09138@gmail.com>
2019-10-27 22:07:24 +0700

    Add base controller and notification service.

--- BackEnd/InternalApplication/API/API.Common/Controllers/BaseController.cs
+++ BackEnd/InternalApplication/API/API.Common/Controllers/BaseController.cs
@@ -5,14 +5,116 @@ namespace API.Common.Controllers
     using System.Text;
     using System.Collections.Generic;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
+    using System.Security.Claims;
+    using Microsoft.AspNetCore.SignalR;
+    using API.Common.Hubs;
+    using System.Threading.Tasks;
 
+    /// <summary>
+    /// Base controller
+    /// </summary>
     public class BaseController : Controller
     {
+        /// <summary>
+        /// Logger service.
+        /// </summary>
         private readonly ILogger<BaseController> _logger;
+
+        /// <summary>
+        /// Hub context.
+        /// </summary>
+        private readonly IHubContext<NotificationServiceHub> _hubContext;
+
+        protected IHubContext<NotificationServiceHub> HubContext => _hubContext;
+
+        /// <summary>
+        /// Connection mapping.
+        /// </summary>
+        private readonly ConnectionMapping _connectionMapping;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="provider">Service provider.</param>
         public BaseController(IServiceProvider provider)
         {
-            //_logger = provider.GetService<ILogger<BaseController>>();
+            this._logger = provider.GetService<ILogger<BaseController>>();
+            this._hubContext = provider.GetService<IHubContext<NotificationServiceHub>>();
+            this._connectionMapping = provider.GetService<ConnectionMapping>();
         }
+
+        /// <summary>
+        /// Get current user id.
+        /// </summary>
+        /// <returns>user id.</returns>
+        protected string CurrentUserId()
+        {
+            if(this.User != null && this.User.Identity.IsAuthenticated)
+            {
+                var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                return userId;
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Send notify to all user except the current user.
+        /// </summary>
+        /// <param name="eventName">Event name.</param>
+        /// <param name="item">Object data.</param>
+        /// <returns>Void.</returns>
+        protected async Task NotifyExceptCurrentUser(string eventName, object item)
+        {
+            try
+            {
+                var connectionIds = this._connectionMapping.GetConnections(this.CurrentUserId());
+                await this._hubContext.Clients.AllExcept(connectionIds).SendAsync(eventName, item);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"NotifyExceptCurrentUser {ex}");
+            }
+        }
+
+        /// <summary>
+        /// Send notify to all user
+        /// </summary>
+        /// <param name="eventName">Event name.</param>
+        /// <param name="item">Object data.</param>
+        /// <returns>Void.</returns>
+        protected async Task NotifyToAll(string eventName, object item)
+        {
+            try
+            {
+                await this._hubContext.Clients.All.SendAsync(eventName, item);
+            }
+            catch(Exception ex)
+            {
+                this._logger.LogError($"NotifyToAll {ex}");
+            }
+        }
+
+        /// <summary>
+        /// Send notify to single user
+        /// </summary>
+        /// <param name="userId">User id</param>
+        /// <param name="eventName">Event name</param>
+        /// <param name="item">Object data</param>
+        /// <returns>Void.</returns>
+        protected async Task NotifyToSingleUser(string userId, string eventName, object item)
+        {
+            try
+            {
+                var connectionIds = _connectionMapping.GetConnections(userId);
+                await _hubContext.Clients.Clients(connectionIds).SendAsync(eventName, item);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("NotifyToSingleUser=" + ex);
+            }
+        }
+
     }
 }
