3e09b03ad6a0aa802a338111a8c6bec9fda9453e
dinhhung09138 <dinhhung09138@gmail.com>
2019-11-30 10:35:31 +0700

    Add event names

---
+++ BackEnd/InternalApplication/InternalApplication/Constants/EventConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace InternalApplication.Constants
+{
+    /// <summary>
+    /// List of event for notification.
+    /// </summary>
+    public static class EventConstants
+    {
+        /// <summary>
+        /// Raise event after login success. For demo only.
+        /// </summary>
+        public static readonly string LoginSuccess = "loginSuccess";
+    }
+}
